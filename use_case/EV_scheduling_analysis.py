0e388289bb5930d496fcbbbae7afc1a32a3fdd7c
petedmccallum <49063588+petedmccallum@users.noreply.github.com>
2026-02-02 13:43:44 +0000

    Updated use_case demo for latest version
    Replace single sim instance with two explicit dres initializations (sim_mod -> 'simulation-1-spaghetti', sim_data -> 'simulation-2-reflex') and update all subsequent calls to use the appropriate instance. Remove redundant EV data load for processed_ev_usage.csv, rename sim_reflex to sim_data, and adjust minor docstring/comment wording and formatting. These changes make the notebook target specific simulation YAMLs and clarify which dataset is used for each analysis section.

--- use_case/EV_scheduling_analysis.py
+++ use_case/EV_scheduling_analysis.py
@@ -1,19 +1,18 @@
 # %% [markdown]
-# # EV Scheduling
+# # Analysys of Spaghetti Model data and real data
 
 # %% [markdown]
-# Load the `dres` package. (If this fails, `dres` hasn't been installed yet - add a temporary cell below and run `!pip install -e .`, after which the cell can be deleted)
+# ### Load `dres` package
 
 # %%
 import dres
 
 # %% [markdown]
-# Initialise `dres`. Various functions and properties can be accessed through `sim.{tab-completion}`. For example:
+# Initialise `dres` instance(s) - include single positional argument directing the call towards the appropriate `simulation-#.yaml`. Once initialised, various functions and properties can be accessed through `sim.{tab-completion}`. For example:
 
 # %%
-sim = dres.go()
-sim.ev.load_ev_data(ev_data_file='ev_schedule_data_reflex.csv', type_of_schedule='charge_event') # Load
-sim.ev.schedule_data                                            # View all data
+sim_mod = dres.go('simulation-1-spaghetti')
+sim_data = dres.go('simulation-2-reflex')
 
 # %% [markdown]
 # ---
@@ -25,17 +24,10 @@ sim.ev.schedule_data                                            # View all data
 # ### 1.1 Examine patterns in arrival/departure times and duration unplugged (single vehicle)
 
 # %% [markdown]
-# We can choose to load specific EV data, to inspect what it contains, before use this data:
-
-# %%
-sim.ev.load_ev_data(ev_data_file='processed_ev_usage.csv', type_of_schedule='journey')      # Load
-sim.ev.schedule_data                                            # View all data
-
-# %% [markdown]
 # For a single EV, we want to see histograms of all departure (unplug) and arrival (plug-in) events, along with a heatmapped across the full time range, showing the number of hours day that the vehicle is unplugged.
 
 # %%
-sim.plots.ev.plot_departure_histogram()
+sim_mod.plots.ev.plot_departure_histogram()
 
 # %% [markdown]
 # ### 1.2 State of charge (single vehicle)
@@ -44,22 +36,21 @@ sim.plots.ev.plot_departure_histogram()
 # #### 1.2.1 Estimate EV Charge Rate (CR)
 
 # %% [markdown]
-# Using the arrive/depart events, find the maxium charge gradient steepness. 
+# Using the arrive/depart events, find the maxium charge gradient steepness.
 # 
 # This is intended to highlight occasions when the EV is unplugged before it is fully charged. On these occasions, we know that the SOC was still increasing at the moment before the EV was unplugged, we can therefore isolate a known time period and a known SOC differential - hence the estimated charge rate (CR).
 # 
 # This does not account for smart charging (i.e. this routine may under-estimate the CR on occasions where the charging process is interrupted by a control signal, but it cannot over-estimate the CR); however, if we find a notable distribution of high-valued CR on the right-hand side of the histogram, this gives a good estimate of the maximum CR.
 
 # %%
-sim.ev.load_ev_data(ev_data_file='processed_ev_usage.csv', type_of_schedule='journey')      # Load
-sim.plots.ev.ev_charge_rate()
+sim_mod.plots.ev.ev_charge_rate()
 
 # %% [markdown]
 # #### 1.2.2 Plot EV SoC timeline
 
 # %%
 max_ev_charge_rate = 0.000137
-sim.plots.ev.ev_soc_timeline(max_ev_charge_rate)
+sim_mod.plots.ev.ev_soc_timeline(max_ev_charge_rate)
 
 # %% [markdown]
 # ---
@@ -71,15 +62,10 @@ sim.plots.ev.ev_soc_timeline(max_ev_charge_rate)
 # ### 2.1 Examine patterns in arrival/departure times and duration unplugged (single vehicle)
 
 # %%
-sim_reflex = dres.go()
-sim_reflex.ev.load_ev_data(ev_data_file='ev_schedule_data_reflex.csv', type_of_schedule='charge_event') # Load
-sim_reflex.ev.schedule_data                                            # View all data
+sim_data.ev.list_most_common_ev_entries()
 
 # %%
-sim_reflex.ev.list_most_common_ev_entries()
-
-# %%
-sim_reflex.plots.ev.plot_departure_histogram(bin_minutes=10, ev_id='EV0065')
+sim_data.plots.ev.plot_departure_histogram(bin_minutes=10, ev_id='EV0065')
 
 # %% [markdown]
 # ### 2.2 State of charge (single vehicle)
@@ -88,14 +74,14 @@ sim_reflex.plots.ev.plot_departure_histogram(bin_minutes=10, ev_id='EV0065')
 # #### 2.2.1 Estimate EV Charge Rate (CR)
 
 # %%
-sim_reflex.plots.ev.ev_charge_rate(ev_id='EV0065')
+sim_data.plots.ev.ev_charge_rate(ev_id='EV0065')
 
 # %% [markdown]
 # #### 2.2.2 Plot EV SoC timeline
 
 # %%
-max_ev_charge_rate = 0.00000001775 # small value forced
-sim_reflex.plots.ev.ev_soc_timeline(max_ev_charge_rate, ev_id='EV0094')
+max_ev_charge_rate = 0.00000001775  # small value forced
+sim_data.plots.ev.ev_soc_timeline(max_ev_charge_rate, ev_id='EV0094')
 
 # %%
 
